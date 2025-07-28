c1be7052731d714eeef72a240aab7523d37c5d9a
Nurjahan Akther Mukta <nurjahanakther433@gmail.com>
2025-07-28 17:24:48 +0600

    Initial commit

---
+++ app/_layout.tsx
@@ -0,0 +1,19 @@
+import { ConvexProvider, ConvexReactClient } from "convex/react";
+import { Stack } from "expo-router";
+export default function RootLayout() {
+  const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL!, {
+  unsavedChangesWarning: false,
+});
+
+  return (
+    
+ <ConvexProvider client={convex}>
+    <Stack screenOptions={{
+       headerShown: false
+        }}>
+      <Stack.Screen name="index" />
+    </Stack>
+     </ConvexProvider>
+  );
+}
+
